d7df62227ab275d45903052900293c5c26484cf4
Romith Jinawardana <romith.jinawardana@sysco.com>
2019-07-18 22:31:17 -0500

    added client

--- src/org/fourneth/FirstTest.groovy
+++ src/org/fourneth/FirstTest.groovy
@@ -1,9 +1,12 @@
 package org.fourneth
 
+import org.kohsuke.github.*
+
 class FirstTest { 
     String name;
 
     String whatsTheDeal() {
+        GitHub github = GitHub.connect('x', 'x');
         return "Deal is ${name}"
     }
 }

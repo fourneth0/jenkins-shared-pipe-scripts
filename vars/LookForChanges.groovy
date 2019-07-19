169668372666ee6fd569e84aff71c53ffbae2f02
Romith Jinawardana <romith.jinawardana@sysco.com>
2019-07-19 00:13:37 -0500

    get the minimum

--- vars/LookForChanges.groovy
+++ vars/LookForChanges.groovy
@@ -4,8 +4,8 @@ import groovy.transform.Field
 
 void call(Map parameters = [:], Closure body = null) {
     echo "Running step name ${STEP_NAME}"
-    echo  params
-    withCredentials([usernamePassword(credentialId: 'username-password-id1', passwordVariable: 'pwd', usernameVariable: 'username')]) {
-        echo username
-    }
+//    echo  params
+//    withCredentials([usernamePassword(credentialId: 'username-password-id1', passwordVariable: 'pwd', usernameVariable: 'username')]) {
+//        echo username
+//    }
 }

2bea731e95c4af8acbc962cf40871dfabde0a040
Romith Jinawardana <romith.jinawardana@sysco.com>
2019-07-23 00:03:40 -0500

    Added more scripts

--- vars/createPR.groovy
+++ vars/createPR.groovy
@@ -3,6 +3,7 @@ import org.fourneth.*
 void call(script) {
     echo "Starting to create PR ${script.promotionConfig.config}"
     def request = new PromotionRequest(script.promotionConfig.config)
+    echo "Going to create"
     request.createPR()
     echo "PR Created"
 }
